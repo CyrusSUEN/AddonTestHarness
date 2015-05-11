5c207fdc4765b8d71d630527d8d1570090b17df5
Daniel Howe <daniel@rednoise.org>
2015-05-12 07:00:38 +0800

    added simple test

---
+++ src/test/java/TestAdNauseam1.java
@@ -0,0 +1,13 @@
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class TestAdNauseam1 {
+
+	@Test
+	public void testHelloWorld() {
+
+		Assert.assertEquals("Hello World", "Hello World!");
+	}
+
+}
